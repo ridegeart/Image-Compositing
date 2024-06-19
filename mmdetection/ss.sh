7c76c8f974a14db783b489b194fa192296cc22c1
chienTang <73794853+ridegeart@users.noreply.github.com>
2024-06-19 09:49:42 +0800

    Delete mmdetection directory

--- mmdetection/ss.sh
+++
@@ -1,7 +0,0 @@
-filename="Faster_Swin_l_w12_DeformRoI_ms_3.pth"
-file_id="1B9FPVqvhPGbXgUdS2eRKIUP2M5zhnhES"
-query=`curl -c ./cookie.txt -s -L "https://drive.google.com/uc?export=download&id=${file_id}" \
-| perl -nE'say/uc-download-link.*? href="(.*?)\">/' \
-| sed -e 's/amp;//g' | sed -n 2p`
-url="https://drive.google.com$query"
-curl -b ./cookie.txt -L -o ${filename} $url
